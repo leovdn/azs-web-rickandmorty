dad1b794c5b521b7a361fef64c52a23d57608c51
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2023-10-26 00:12:15 -0300

    feat: search episode by name

--- components/EpisodeList.tsx
+++ components/EpisodeList.tsx
@@ -5,14 +5,19 @@ import { Episode } from '../types/types'
 
 export default function EpisodeList() {
   const { episodes, loading, error } = useEpisodeContext()
+
   const [favorites, setFavorites] = useState<number[]>(() => {
     const favoritesFromStorage = localStorage.getItem('favorites')
     return favoritesFromStorage ? JSON.parse(favoritesFromStorage) : []
   })
+
   const [watched, setWatched] = useState<number[]>(() => {
     const watchedFromStorage = localStorage.getItem('watched')
     return watchedFromStorage ? JSON.parse(watchedFromStorage) : []
   })
+
+  const [searchQuery, setSearchQuery] = useState('')
+  const [filteredEpisodes, setFilteredEpisodes] = useState<EpisodeType[]>([])
 
   const toggleFavorite = (id: number) => {
     const index = favorites.indexOf(id)
@@ -40,30 +45,50 @@ export default function EpisodeList() {
     localStorage.setItem('watched', JSON.stringify(watched))
   }, [watched])
 
+  // Update filtered episodes when the search query changes
+  useEffect(() => {
+    const filtered = episodes.filter((episode) =>
+      episode.name.toLowerCase().includes(searchQuery.toLowerCase())
+    )
+    setFilteredEpisodes(filtered)
+  }, [episodes, searchQuery])
+
   if (loading) return <p>Loading...</p>
 
   if (error) return <p>Something went wrong</p>
 
   return (
     <div>
-      {episodes.map((episode: Episode) => (
-        <div key={episode.id} style={{ border: '2px solid' }}>
-          <button onClick={() => toggleFavorite(episode.id)}>
-            {favorites.includes(episode.id) ? 'Unfavorite' : 'Favorite'}
-          </button>
+      {/* Search input field */}
+      <input
+        type="text"
+        placeholder="Search by episode name"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
 
-          <button onClick={() => toggleWatched(episode.id)}>
-            {watched.includes(episode.id) ? 'Unwatched' : 'Watched'}
-          </button>
+      {filteredEpisodes.length === 0 ? (
+        <p>No episodes found.</p>
+      ) : (
+        filteredEpisodes.map((episode: Episode) => (
+          <div key={episode.id} style={{ border: '2px solid' }}>
+            <button onClick={() => toggleFavorite(episode.id)}>
+              {favorites.includes(episode.id) ? 'Unfavorite' : 'Favorite'}
+            </button>
 
-          <Link to={`episode/${episode.id}`}>
-            <h2>{episode.name}</h2>
-            <p>{episode.air_date}</p>
-            <p>{episode.episode}</p>
-            <p>{episode.characters.length} characters</p>
-          </Link>
-        </div>
-      ))}
+            <button onClick={() => toggleWatched(episode.id)}>
+              {watched.includes(episode.id) ? 'Unwatched' : 'Watched'}
+            </button>
+
+            <Link to={`episode/${episode.id}`}>
+              <h2>{episode.name}</h2>
+              <p>{episode.air_date}</p>
+              <p>{episode.episode}</p>
+              <p>{episode.characters.length} characters</p>
+            </Link>
+          </div>
+        ))
+      )}
     </div>
   )
 }
