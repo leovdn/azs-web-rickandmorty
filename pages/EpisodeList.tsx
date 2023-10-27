55963cf09dbaedcf9358b766c51efbccf8884ccc
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2023-10-27 00:51:41 -0300

    feat: create Characters Card component

--- pages/EpisodeList.tsx
+++ pages/EpisodeList.tsx
@@ -1,10 +1,9 @@
-import { Link } from 'react-router-dom'
 import { useEpisodeContext } from '../context/EpisodeContext'
 import { Episode } from '../types/types'
-import FavoriteButton from '../components/FavoriteButton'
-import WatchedButton from '../components/WatchedButton'
+
 import SearchInput from '../components/SearchInput'
 import useSearch from '../hooks/useSearch'
+import { EpisodeCard } from '../components/EpisodeCard'
 
 export default function EpisodeList() {
   const { episodes, loading, error } = useEpisodeContext()
@@ -21,21 +20,13 @@ export default function EpisodeList() {
   return (
     <div>
       <SearchInput value={searchQuery} onChange={setSearchQuery} />
+
       {filteredItems.length === 0 ? (
         <p>No episodes found.</p>
       ) : (
         filteredItems.map((episode: Episode) => (
           <div key={episode.id} style={{ border: '2px solid' }}>
-            <FavoriteButton episodeId={episode.id} />
-
-            <WatchedButton episodeId={episode.id} />
-
-            <Link to={`episode/${episode.id}`}>
-              <h2>{episode.name}</h2>
-              <p>{episode.air_date}</p>
-              <p>{episode.episode}</p>
-              <p>{episode.characters.length} characters</p>
-            </Link>
+            <EpisodeCard episode={episode} />
           </div>
         ))
       )}
